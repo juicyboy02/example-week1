9a209906574c92d69ffde70dcfa14b881abd07bf
威威 <juicyboy02@gmail.com>
2014-05-01 23:46:15 +0800

    - Adding stylesheet and changing the js

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-/**
- * @overview
- *
- * @author
- * @version 2014/04/26
- */
-
 var http = require("http");
 var port = 1337;
 var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+var url = "http://graph.facebook.com/hypebeast/photos?type=uploaded";
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head><title>hello world</title></head><body>"
+  var data = "<html><head><title>hypebeast</title><link rel='stylesheet' href='mystyle.css'></head><body>"
+  data += "<div id='wrapper'><div id='columns'>"
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+      data += "<div class='pin'><img src='" + val.images[2].source + "'></div>";
     });
     
-    data += "</body></html>";
+    data += "</div></div></body></html>";
     res.end(data);
   });
 
 }).listen(port);
 
 console.log("start server port: " + port);
-
-
